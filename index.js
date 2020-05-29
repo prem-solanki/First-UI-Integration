982bb38be709c33ded37ba0e6b539053934a0a4f
prem-solanki <solanki_prem@network.elanco.com>
2020-05-29 18:52:36 +0530

    First UI

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const handlebars = require('express-handlebars');
+//Sets our app to use the handlebars engine
+app.set('view engine', 'handlebars');
+//Sets handlebars configurations (we will go through them later on)
+app.engine('handlebars', handlebars({
+layoutsDir: __dirname + '/views/layouts',
+}));
+
+app.get('/', (req, res) => {
+    //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+    res.render('main', {layout : 'index'});
+    
+    });
+app.use(express.static('public'))
+app.listen(3000, () => console.log('Server started...'));
+
